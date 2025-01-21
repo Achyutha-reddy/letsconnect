04f416e525b0e74f0b5d9fb1ac38217ccc0191ce
Achyutha Reddy <achyuthareddyborampeta@gmail.com>
2025-01-21 09:23:44 +0530

    About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -30,7 +30,7 @@ const About = () => {
             <article className='about__card'>
               <VscFolderLibrary  className='about__icon'/>
               <h5>PROJECTS</h5>
-              <small>2 PROJECTS</small>
+              <small>6 PROJECTS</small>
             </article>
           </div>
           <p>Coming from a background in design, I love creating pages where I’m able to actually create enjoyable interactions and experiences for everyone!I possess potential across a wide range of technical skills, including programming languages such as Python, C as well as expertise in utilizing tools and frameworks such as Git, Docker for efficient project development and collaboration. Familiarity with database management systems, including SQL. Possess a strong understanding of networking protocols, web development technologies (HTML/CSS, JavaScript, React).
